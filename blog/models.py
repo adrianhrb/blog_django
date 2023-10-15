bf8008d91c36ff593a13ff8f510b68a2c50c1908
adrianhrb <adrian.herrera.br@gmail.com>
2023-10-15 17:17:33 +0100

    Chapter 1 done

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,22 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 
 
+# Manager to filter only published Posts
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
+    # Status field possible values
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
+
+    # Model fields
 
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=256)
@@ -16,6 +26,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    objects = models.Manager()  # Default manager
+    published = PublishedManager()  # Our custom manager
 
     class Meta:
         """Defines metadata for the model with some attributes"""
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    """This view display all the published posts on the blog"""
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    """This function display a the single post that is requested by the user"""
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})
