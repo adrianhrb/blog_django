d248f92d6afb38b3145f95edd432d834d4b6e711
adrianhrb <adrian.herrera.br@gmail.com>
2023-10-15 16:36:08 +0100

    User imported

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=256)
     body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
     class Meta:
         """Defines metadata for the model with some attributes"""
