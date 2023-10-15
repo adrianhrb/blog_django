21e0b11536a0f628711e3b680ae4d779401f6c2b
adrianhrb <adrian.herrera.br@gmail.com>
2023-10-15 17:44:02 +0100

    Pagination working

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 
 from blog.models import Post
@@ -5,11 +6,27 @@ from blog.models import Post
 
 def post_list(request):
     """This view display all the published posts on the blog"""
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year: int, month: int, day: int, post: Post):
     """This function display a the single post that is requested by the user"""
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     return render(request, 'blog/post/detail.html', {'post': post})
