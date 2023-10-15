d8362222d7895bfbdea57eda8742c9f93aab5388
adrianhrb <adrian.herrera.br@gmail.com>
2023-10-15 21:08:40 +0100

    Comments and mail system working

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 from blog import views
 
 app_name = 'blog'
+# In case of using post_list by using class, the path will be:"
+# path('', views.PostListView.as_view(), name='post_list')
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
+from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 
+from blog.forms import CommentForm, EmailPostForm
 from blog.models import Post
+
+# from django.views.generic import ListView
+
+
+# class PostListViews(ListView):
+#     """Alternative post_list view by using a class, not a function"""
+#
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 
 def post_list(request):
@@ -29,4 +43,50 @@ def post_detail(request, year: int, month: int, day: int, post: Post):
         publish__month=month,
         publish__day=day,
     )
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(
+        request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form}
+    )
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        # In this step form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed the validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f'{cd["name"]} recommends you to read {post.title}'
+            message = (
+                f'Read {post.title} at {post_url}\n\n{cd["name"]}\'s comments: {cd["comments"]}'
+            )
+            send_mail(subject, message, 'adrian.herrera.br@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment}
+    )
